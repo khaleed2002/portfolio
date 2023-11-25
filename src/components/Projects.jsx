b668127d9e4177c7bbac346dd73f65d425783924
khaleed2002 <zekomadred159@gmail.com>
2023-11-25 16:53:43 +0200

    update grid col

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,10 +3,10 @@ import { projects } from '../data'
 import ProjectCard from './ProjectCard'
 const Projects = () => {
   return (
-    <section className="py-16" id='projects'>
+    <section className="py-16" id="projects">
       <div className="align-element">
         <Title text="Web Creations" />
-        <ul className="py-16 grid md:grid-cols-3 lg:grid-col-3 gap-8">
+        <ul className="py-16 grid md:grid-cols-2 lg:grid-col-3 gap-8">
           {projects.map((project) => {
             return <ProjectCard key={project.id} {...project} />
           })}
