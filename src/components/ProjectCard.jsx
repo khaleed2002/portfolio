57a5b14025b9acb9729c5f8142533cffefdf91bc
khaleed2002 <zekomadred159@gmail.com>
2023-11-25 16:18:44 +0200

    add project

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,17 +1,29 @@
 import { FaGithubSquare } from 'react-icons/fa'
 import { TbWorldWww } from 'react-icons/tb'
-const ProjectCard = ({ img, url, github, title, text }) => {
+const ProjectCard = ({ id, img, url, github, title, technologies }) => {
   return (
     <li className=" overflow-hidden rounded-t-lg bg-white drop-shadow-lg hover:drop-shadow-2xl duration-300">
-      <img src={img} alt="Project preview img" className="w-full h-64" />
+      <img
+        src={img}
+        alt="Project preview img"
+        className="w-full h-64 object-cover object-center"
+      />
       <div className=" p-8 flex flex-col gap-8 ">
-        <h3 className=" font-bold text-xl capitalize">{title}</h3>
-        <p className=" text-slate-700">{text}</p>
+        <h3 className=" font-bold text-2xl capitalize">{title}</h3>
+        <div className="flex flex-wrap gap-4">
+          {technologies.map((t) => {
+            return (
+              <p key={id} className=" rounded-xl bg-red-800 text-white p-2">
+                {t}
+              </p>
+            )
+          })}
+        </div>
         <div className="flex gap-4">
-          <a href={url}>
+          <a href={url} target="_blank">
             <TbWorldWww className="icon" />
           </a>
-          <a href={github}>
+          <a href={github} target="_blank">
             <FaGithubSquare className="icon" />
           </a>
         </div>
